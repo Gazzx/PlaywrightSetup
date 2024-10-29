3c1b90503d51511b44a8481f887089cd3858568c
BAUDUIN Raphaël <raphael@asfaload.com>
2024-10-29 11:08:27 +0100

    initial commit

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+
+let installDeps () =
+    let exitCode = Microsoft.Playwright.Program.Main([| "install-deps" |])
+
+    if (exitCode <> 0) then
+        Console.WriteLine("Failed to install browsers dependencies")
+        Environment.Exit(exitCode)
+
+let installBrowsers () =
+    let exitCode = Microsoft.Playwright.Program.Main([| "install" |])
+
+    if (exitCode <> 0) then
+        Console.WriteLine("Failed to install browsers")
+        Environment.Exit(exitCode)
+
+installDeps ()
+installBrowsers ()
